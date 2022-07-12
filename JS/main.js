e814478a77ca75fba29345a55e6736a497349a79
Frey Usuga <fausuga673@misena.edu.co>
2022-07-12 18:23:39 -0500

    Código js optimizado

--- JS/main.js
+++ JS/main.js
@@ -2,7 +2,6 @@ const rock = document.getElementById("control__rock");
 const paper = document.getElementById("control__paper");
 const scissor = document.getElementById("control__scissor");
 const messege = document.getElementById("messege");
-const tieMessege = (messege.innerHTML = "Empate.", messege.style.color = "var(--color-white)");
 
 rock.addEventListener("click", function() {
     playerMove(rock);
@@ -16,27 +15,38 @@ scissor.addEventListener("click", function() {
     playerMove(scissor);
 })
 
+function tieMessege() {
+    return messege.innerHTML = "Empate.", messege.style.color = "var(--color-white)";
+}
+function loseMessege() {
+    return messege.innerHTML = "Perdiste...", messege.style.color = "var(--color-red)";
+}
+
+function winMessege() {
+    return messege.innerHTML = "¡Tú ganas!", messege.style.color = "var(--color-green)";
+}
+
 function playerMove(move) {
 
     let enemy = Math.floor(Math.random() * 3 + 1);
 
     if (move == rock) {
 
-        if (enemy == 1) {console.log(tieMessege);}
-        if (enemy == 2) {console.log(messege.innerHTML = "Perdiste..."), messege.style.color = "var(--color-red)";}
-        if (enemy == 3) {console.log(messege.innerHTML = "¡Tú ganas!", messege.style.color = "var(--color-green)");}
+        if (enemy == 1) {tieMessege();}
+        if (enemy == 2) {loseMessege();}
+        if (enemy == 3) {winMessege();}
 
     } else if (move == paper) {
 
-        if (enemy == 1) {console.log(messege.innerHTML = "¡Tú ganas!", messege.style.color = "var(--color-green)");}
-        if (enemy == 2) {console.log(messege.innerHTML = "Empate.", messege.style.color = "var(--color-white)");}
-        if (enemy == 3) {console.log(messege.innerHTML = "Perdiste...", messege.style.color = "var(--color-red)");}
+        if (enemy == 1) {winMessege();}
+        if (enemy == 2) {tieMessege();}
+        if (enemy == 3) {loseMessege();}
 
     }  else if (move == scissor) {
 
-        if (enemy == 1) {console.log(messege.innerHTML = "Perdiste...", messege.style.color = "var(--color-red)");}
-        if (enemy == 2) {console.log(messege.innerHTML = "¡Tú ganas!", messege.style.color = "var(--color-green)");}
-        if (enemy == 3) {console.log(messege.innerHTML = "Empate.", messege.style.color = "var(--color-white)");}
+        if (enemy == 1) {loseMessege();}
+        if (enemy == 2) {winMessege();}
+        if (enemy == 3) {tieMessege();}
 
     }
 
