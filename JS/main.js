60ba3b9641c6b3729b7d584a2ee201cbe4c74e94
Frey Usuga <fausuga673@misena.edu.co>
2022-07-12 18:04:56 -0500

    código optimizado

--- JS/main.js
+++ JS/main.js
@@ -2,6 +2,7 @@ const rock = document.getElementById("control__rock");
 const paper = document.getElementById("control__paper");
 const scissor = document.getElementById("control__scissor");
 const messege = document.getElementById("messege");
+const tieMessege = (messege.innerHTML = "Empate.", messege.style.color = "var(--color-white)");
 
 rock.addEventListener("click", function() {
     playerMove(rock);
@@ -17,11 +18,11 @@ scissor.addEventListener("click", function() {
 
 function playerMove(move) {
 
-    const enemy = Math.floor(Math.random() * 3 + 1);
+    let enemy = Math.floor(Math.random() * 3 + 1);
 
     if (move == rock) {
 
-        if (enemy == 1) {console.log(messege.innerHTML = "Empate.", messege.style.color = "var(--color-white)");}
+        if (enemy == 1) {console.log(tieMessege);}
         if (enemy == 2) {console.log(messege.innerHTML = "Perdiste..."), messege.style.color = "var(--color-red)";}
         if (enemy == 3) {console.log(messege.innerHTML = "¡Tú ganas!", messege.style.color = "var(--color-green)");}
 
