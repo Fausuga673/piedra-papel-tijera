86f87517e659b3933acd2d016bc33f1909259603
Frey Usuga <fausuga673@misena.edu.co>
2022-07-13 14:31:52 -0500

    código js optimizado y se creó la carpeta sound, dentro de esta el archivo rocket.mp3

--- JS/main.js
+++ JS/main.js
@@ -1,18 +1,22 @@
 const rock = document.getElementById("control__rock");
 const paper = document.getElementById("control__paper");
 const scissor = document.getElementById("control__scissor");
+
+const playerImgMove = document.getElementById("player__move");
+const enemyImgMove = document.getElementById("enemy__move");
+
 const messege = document.getElementById("messege");
 
 rock.addEventListener("click", function() {
-    playerMove(rock);
+    combat(rock);
 })
 
 paper.addEventListener("click", function() {
-    playerMove(paper);
+    combat(paper);
 })
 
 scissor.addEventListener("click", function() {
-    playerMove(scissor);
+    combat(scissor);
 })
 
 function tieMessege() {
@@ -26,27 +30,39 @@ function winMessege() {
     return messege.innerHTML = "¡Tú ganas!", messege.className = "winMessege";
 }
 
-function playerMove(move) {
+function combat(move) {
 
     let enemy = Math.floor(Math.random() * 3 + 1);
 
+    if (enemy == 1) enemy = "rock";
+    if (enemy == 2) enemy = "paper";
+    if (enemy == 3) enemy = "scissor";
+    
+    enemyImgMove.src = `img/${enemy}.png`
+
     if (move == rock) {
 
-        if (enemy == 1) {tieMessege();}
-        if (enemy == 2) {loseMessege();}
-        if (enemy == 3) {winMessege();}
-
+        playerImgMove.src=`img/rock.png`;
+        
+        if (enemy == "rock") tieMessege();
+        if (enemy == "paper") loseMessege();
+        if (enemy == "scissor") winMessege();
+        
     } else if (move == paper) {
-
-        if (enemy == 1) {winMessege();}
-        if (enemy == 2) {tieMessege();}
-        if (enemy == 3) {loseMessege();}
-
+        
+        playerImgMove.src="img/paper.png";
+        
+        if (enemy == "rock") winMessege();
+        if (enemy == "paper") tieMessege();
+        if (enemy == "scissor") loseMessege();
+        
     }  else if (move == scissor) {
-
-        if (enemy == 1) {loseMessege();}
-        if (enemy == 2) {winMessege();}
-        if (enemy == 3) {tieMessege();}
+        
+        playerImgMove.src="img/scissor.png";
+        
+        if (enemy == "rock") loseMessege();
+        if (enemy == "paper") winMessege();
+        if (enemy == "scissor") tieMessege();
 
     }
 
